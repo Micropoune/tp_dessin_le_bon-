9ccb2b396df082badaa30543d1ef456ab040d3c2
Mathieu Moisan <mathieu_moisan@hotmail.com>
2018-11-29 17:46:09 -0500

    grgregeg

--- src/ca/csf/dfc/Dessin/Modele/Emplacement.java
+++ src/ca/csf/dfc/Dessin/Modele/Emplacement.java
@@ -15,21 +15,25 @@ public class Emplacement {
 
 	  
 
-	  public void Emplacement(int p_x,  int p_y) {
-	  }
-
-	  public void getX() {
-		  
-	  }
-
-	  public void getY() {
-		  
-	  }
-
-	  public void setX(int p_x) {
-	  }
-
-	  public void setY(int p_y) {
-	  }
+	  public Emplacement() {
+			this.m_X = 0;
+			this.m_Y = 0;
+		}
+		
+		public int getX() {
+			return this.m_X;
+		}
+		
+		public int getY() {
+			return this.m_Y;
+		}
+		
+		public void setX(int p_X) {
+			this.m_X = p_X;
+		}
+		
+		public void setY(int p_Y) {
+			this.m_Y = p_Y;
+		}
 
 	}
