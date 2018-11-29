9f86e0e19f85ace8e6b9b71b97ca077f220c6f90
Mathieu Moisan <mathieu_moisan@hotmail.com>
2018-11-29 18:34:05 -0500

    ffef

--- src/ca/csf/dfc/Dessin/Modele/Rectangle.java
+++ src/ca/csf/dfc/Dessin/Modele/Rectangle.java
@@ -2,20 +2,29 @@
 
 import java.awt.Color;
 import java.awt.Graphics;
+
+import ca.csf.dfc.Dessin.Modele.Emplacement;
 import ca.csf.dfc.Dessin.Modele.FormeGraphique;
 /**
  * @author Sylvie
  *
  */
 public class Rectangle extends FormeGraphique {
-
-	  public Rectangle(Color p_couleurRemplissage, Color p_CouleurContour, int p_valX, int p_valY, int p_largeur, int p_hauteur, Graphics p_Graphic) {
-		  this.setCouleurRemplissage(p_couleurRemplissage);
-		  this.setCouleurContour(p_CouleurContour);
+	
+	private Emplacement m_emplacement;
+	
+	  public Rectangle(Integer p_epaisseur,Color p_couleurRemplissage, Color p_CouleurContour, int p_valX, int p_valY, int p_largeur, int p_hauteur) {
+		  super.setEpaisseur(p_epaisseur);
+		  super.setCouleurRemplissage(p_couleurRemplissage);
+		  super.setCouleurContour(p_CouleurContour);
+		  this.m_emplacement.setX(p_valX);
+		  this.m_emplacement.setY(p_valY);
 	  }
 
-	  public void dessiner() {
-		  
+	  public void dessiner(Graphics p_Graphic) {
+		  if(super.getCouleurRemplissage() == null) {
+			  p_Graphic.drawRect(this.m_emplacement.getX(), this.getEmplacement().getY(), width, height);
+		  }
 	  }
 
 	}
